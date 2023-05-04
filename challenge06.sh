23aec5514308e3a311af7add508da2a17d957afa
Acheyos <vaniwaarden@protonmail.ch>
2023-05-03 20:12:14 -0400

    updated

--- challenge06.sh
+++ challenge06.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-$FILE="challenge06.txt"
+
+
+
+FILE="challenge06.txt"
 
 if [ -f "$FILE" ]
 then
     echo "File $FILE exists."
 else
-    touch $FILE
+    
     echo "File $FILE does not exist."
+
+    echo " Would you like to create challenge06.txt? (Y/N) "
+    read y
+    if [ $y = y ] 
+    then
+    touch challenge06.txt
+    else
+    echo "File is not created."
+    fi
 fi
